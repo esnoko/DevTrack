00b591bb0c3c814a77185acdb56188c79d1c77bb
esnoko <elvisnoko18@gmail.com>
2026-05-21 14:40:12 +0200

    add buildTechStack to module exports for tech stack integration

--- backend/services/builders/index.js
+++ backend/services/builders/index.js
@@ -2,10 +2,12 @@ const { buildRepositorySummary } = require('./repository');
 const { buildLanguageBreakdown } = require('./language');
 const { buildCommitActivity } = require('./commit');
 const { buildRepoQualityIndicators } = require('./quality');
+const { buildTechStack } = require('./techStack');
 
 module.exports = {
   buildRepositorySummary,
   buildLanguageBreakdown,
   buildCommitActivity,
-  buildRepoQualityIndicators
+  buildRepoQualityIndicators,
+  buildTechStack
 };
